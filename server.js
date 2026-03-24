0b3ccaa47c9fe51dd0e9e5fdb392fd64954acca4
dvaaz <darley.azevedo@gmail.com>
2026-03-24 20:47:02 -0300

    fastfy first

--- server.js
+++
@@ -1,9 +0,0 @@
-import app from "./src/index.js"
-// require('dotenv').config()
-
-// const PORT = process.env.PORT || 3000
-const port = 3000
-
-app.listen(port, () => {
-    console.log(`Servidor rodando http://localhost:${port}`)
-})
